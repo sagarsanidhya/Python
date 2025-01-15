026ad8cb3f89781fd5c9ec23906178ca718882e3
sagarsanidhya <sagarsanidhya@gmail.com>
2025-01-16 00:44:32 +0530

    Function Introduction

---
+++ Day 6/functions.py
@@ -0,0 +1,18 @@
+print()  #functions are followed by a set of paranthesis
+
+#own Function start with keyword "def"
+
+def my_function():
+    print("hello")
+
+    #Function not Executed yet
+
+my_function() # function executed not by calling function mane followed by paranthesis
+ 
+
+ #Turn Right Assignment 
+
+#  def turn_right():
+#    turn_left()
+#    turn_left()
+#     turn_left()
